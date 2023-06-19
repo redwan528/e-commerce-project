ccc961a44fa5ba0f1247dd758be682f6e4d9680c
redwankhan <redwankhan144@gmail.com>
2023-06-19 17:05:27 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const formOpenBtn = document.querySelector("#form-open"),
+home = document.querySelector(".home"),
+formContainer = document.querySelector(".form-container"),
+// formCloseBtn = document.querySelector("#form-close"),
+signupBtn = document.querySelector("#signup"),
+loginBtn = document.querySelector("#login"),
+loginBtn = document.querySelector("#login");
+
+
+
+
